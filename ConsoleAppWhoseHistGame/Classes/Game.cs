e0cd0a1fc46b0b32fab2457b29da5bd8e0668b9a
IAnderson-rgb <60550263+IAnderson-rgb@users.noreply.github.com>
2020-06-08 22:29:04 -0400

    Update to Game.cs

--- ConsoleAppWhoseHistGame/Classes/Game.cs
+++ ConsoleAppWhoseHistGame/Classes/Game.cs
@@ -15,6 +15,8 @@ namespace ConsoleAppWhoseHistGame.Classes
         public string Questions { get; private set; }
         public string InputAnswer { get; set; }
         public bool IsNewGameTrue { get; set; }
+        public List<string> Answers { get; private set; }
+        public List<string> PlayerGeusses { get; private set; }
 
         /// <summary>
         /// Creates a new game by setting player info and time to play.
@@ -27,17 +29,26 @@ namespace ConsoleAppWhoseHistGame.Classes
             Timer = seconds;
             IsNewGameTrue = isNewGameTrue;
             PlayerName = playerName;
+            Answers = answers;
+            PlayerGeusses = playerGeusses;
         }
-
+        /// <summary>
+        /// List populated with answers. Contains the Original order of text minus the year the event took place.
+        /// List populated with player's geuss for each scrambled word problem.
+        /// </summary>
+         private List<string> answers = new List<string>();
+         private List<string> playerGeusses = new List<string>();
+        /// <summary>
+        /// Gives the date the user chosses to play. Reads in historical data from a txt file based on the date givin. Reverses the order of the word in the txt file. Creates an answers list of the original word order.
+        /// </summary>
+        /// <param name="selectedDate">Date chosen by the user eg..5-14 </param>
         public void PlayGame(string selectedDate = "")
         {// create a timer to run
             string directory = Environment.CurrentDirectory;
             selectedDate = selectedDate + ".txt";
             string fullPath = Path.Combine(directory, selectedDate);
 
-            List<string> answers = new List<string>();
-            List<string> scrambledQuestion = new List<string>();
-            List<string> playerGeusses = new List<string>();
+            //List<string> scrambledQuestion = new List<string>();
 
             try
             {
@@ -46,7 +57,7 @@ namespace ConsoleAppWhoseHistGame.Classes
                     while (!sr.EndOfStream)
                     {
                         string line = sr.ReadLine();
-                        answers.Add(line);
+                        answers.Add(line.Remove(0,6));
                         string[] phraseArray = line.Split(',');
                         Console.WriteLine();
                         Console.WriteLine(phraseArray[0]);
@@ -57,16 +68,21 @@ namespace ConsoleAppWhoseHistGame.Classes
                         for (int i = 0; i < reversedArray.Length; i++)
                         {
                             //string[] resultArray;
-                            Console.Write(reversedArray[i]); //This allows array of strings to print a single line of string elements to the console.
+                            Console.Write($"{reversedArray[i]},"); //This allows array of strings to print a single line of string elements to the console.
                         }
-
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        Console.WriteLine($"What is your geuss {PlayerName}?");
+                        string geuss = Console.ReadLine();
+                        playerGeusses.Add(geuss);
                     }
                 }
+
             }
             catch (Exception e)
             {
 
-                Console.WriteLine(e.Message); ;
+                Console.WriteLine(e.Message); 
             }
             
         }
