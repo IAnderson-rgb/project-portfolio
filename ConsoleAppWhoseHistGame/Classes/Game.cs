0a337c6d2b0298d95269e11a83f475c95d40b155
IAnderson-rgb <60550263+IAnderson-rgb@users.noreply.github.com>
2020-06-09 17:19:57 -0400

    Submitting update to Game.cs and Unit test

--- ConsoleAppWhoseHistGame/Classes/Game.cs
+++ ConsoleAppWhoseHistGame/Classes/Game.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ConsoleAppWhoseHistGame.Classes
 {
---
+++ ConsoleAppWhoseHistGame/Tests/ResultsTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppWhoseHistGame.Tests
+{
+    class ResultsTest
+    {
+    }
+}
